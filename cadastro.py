bff4dba3c0d3b88471a28ce9cedf9580a80e3501
Arnaldo Pereira Silva <arnaldopereirasilva99@gmail.com>
2022-06-11 12:28:19 -0300

    Criação de classes

---
+++ cadastro.py
@@ -0,0 +1,33 @@
+class Pessoa:
+    def __init__(self, nome, fone):
+        self.nome = nome
+        self.fone = fone
+
+
+class Squad:
+    def __init__(self, nome, techlead=None, devs=None):
+        self.nome = nome
+        self.devs = []
+        self.techlead = techlead
+
+    def incluir_techlead(self, techlead):
+        self.techlead = techlead
+
+    def incluir_dev(self, dev):
+        self.devs.append(dev)
+
+
+class Colaborador(Pessoa):
+    def __int__(self, nome, fone, squad=None):
+        super().__init__(nome, fone)
+        self.squad = squad
+
+    def incluir_squad(self, squad):
+        self.squad = squad
+
+class Dev(Colaborador):
+    def __int__(self, nome, fone, cargo, squad=None):
+        super().__int__(nome, fone, squad)
+        self.cargo = cargo
+
+
