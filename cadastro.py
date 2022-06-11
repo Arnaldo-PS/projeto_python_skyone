c06d1385d594cd68d97245c1e8cf0fa60f32e235
Arnaldo Pereira Silva <arnaldopereirasilva99@gmail.com>
2022-06-11 13:12:31 -0300

    add função while

--- cadastro.py
+++ cadastro.py
@@ -18,16 +18,45 @@ class Squad:
 
 
 class Colaborador(Pessoa):
-    def __int__(self, nome, fone, squad=None):
+    def __init__(self, nome, fone, squad=None):
         super().__init__(nome, fone)
         self.squad = squad
 
     def incluir_squad(self, squad):
         self.squad = squad
 
+
 class Dev(Colaborador):
-    def __int__(self, nome, fone, cargo, squad=None):
-        super().__int__(nome, fone, squad)
+    def __init__(self, nome, fone, cargo, squad=None):
+        super().__init__(nome, fone, squad)
         self.cargo = cargo
 
 
+while True:
+    squads = []
+    nome_squad = input('\nNome da squad: ')
+    nome_techlead = input('Nome do techlead: ')
+    fone_techlead = input('Telefone do techlead: ')
+
+    squad = Squad(nome_squad)
+    techlead = Colaborador(nome_techlead, fone_techlead)
+    squad.incluir_techlead(techlead)
+    techlead.incluir_squad(squad)
+
+    squads.append(squad)
+
+    while True:
+        nome_dev = input('\nNome do desenvolvedor: ')
+        fone_dev = input('Telefone do desenvolvedor: ')
+        cargo_dev = input('Cargo do desenvolvedor: ')
+        dev = Dev(nome_dev, fone_dev, cargo_dev)
+        dev.incluir_squad(squad)
+        squad.incluir_dev(dev)
+
+        option = input('\nDeseja adicionar mais um dev [S/N]: ')
+        if option in 'Nn':
+            break
+
+    option = input('\nDeseja adicionar mais uma squad [S/N]: ')
+    if option in 'Nn' :
+        break
